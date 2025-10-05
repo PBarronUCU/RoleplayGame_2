f15a7bce77c9033a28cbcdb6f05b53ec1df0044e
Patricio <PATRICIO.BARRON@correo.ucu.edu.uy>
2025-10-05 15:10:08 -0300

    Implementada Clase Mago

--- src/Library/Characters/Mago.cs
+++ src/Library/Characters/Mago.cs
@@ -1,6 +1,125 @@
+using Library.Items;
+
 namespace Library.Characters;
 
 public class Mago : IPersonajeMagico
 {
+    public string Nombre {get; set;}
+    public double Vida { get; set; } = 100;
+    private List<IItemNoMag> _listaitems = new List<IItemNoMag>();
+    private List<IItemMag> _listaitemsmag = new List<IItemMag>();
+    public Mago(string name)
+    {
+        this.Nombre = name;
+    }
+
+    private Libro _libro;
+
+    public double AtaqueTotal
+    {
+        get
+        {
+            double resultado = 0;
+            foreach (var item in _listaitems)
+            {
+                if (item is IIAtaqueNoMag)
+                {
+                    resultado += item.ValorAtaque;
+                }
+            }
+
+            foreach (var itemMagico in _listaitemsmag)
+            {
+                if (itemMagico is IIAtaqueMag)
+                {
+                    resultado += itemMagico.ValorAtaque;
+                }
+            }
+            
+            return resultado;
+        }
+    }
+
+    public double DefensaTotal
+    {
+        get
+        {
+            double resultado = 0;
+            foreach (var item in _listaitems)
+            {
+                if (item is IIDefensaNoMag)
+                {
+                    resultado += item.ValorDefensa;
+                }
+            }
+
+            foreach (var itemMagico in _listaitemsmag)
+            {
+                if (itemMagico is IIDefensaMag)
+                {
+                    resultado += itemMagico.ValorDefensa;
+                }
+            }
+            
+            return resultado;
+        }
+    }
+
+    public void Atacar(IPersonaje personaje)
+    {
+        double danio = this.AtaqueTotal;
+        double defensa = personaje.DefensaTotal;
+        double danio_resultante = 0;
+        if (defensa > danio)
+        {
+            danio_resultante = 0;
+        }
+        else
+        {
+            danio_resultante = danio - defensa; 
+        }
+        if (danio_resultante >= personaje.Vida)
+        {
+            personaje.Vida = 0;
+        }
+        else
+        {
+            personaje.Vida -= danio_resultante;
+        }
+    }
+
+    public void Curar(IPersonaje personaje)
+    {
+        personaje.Vida = 100;
+    }
+
+    public void AgregarItem(IItemNoMag item)
+    {
+        _listaitems.Add(item);
+    }
+
+    public void Quitar_Item(IItemNoMag item)
+    {
+        _listaitems.Remove(item);
+    }
     
+    public void AgregarItemMag(IItemMag item)
+    {
+        _listaitemsmag.Add(item);
+    }
+    
+    public void QuitarItemMag(IItemMag item)
+    {
+        _listaitemsmag.Remove(item);
+    }
+    
+    public void AgregarLibro(Libro libro)
+    {
+        _libro = libro;
+    }
+    
+    public void QuitarLibro(Libro libro)
+    {
+        _libro = null;
+    }
 }
